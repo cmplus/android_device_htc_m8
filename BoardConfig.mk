c8178441758c8700a323e84fed1a4c70690b7650
sspencer10 <sspencer10@gmail.com>
2014-06-19 08:12:58 -0500

    use linaro

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,7 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 -
 BOARD_CUSTOM_BOOTIMG_MK := device/htc/m8/mkbootimg.mk
 TARGET_KERNEL_CONFIG := cm_m8_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/msm8974
+TARGET_GCC_VERSION_ARM := 4.9
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
